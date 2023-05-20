b60a93bc4e4375f9e55251b357c00ed071e4ea97
newazcs <newazcs@gmail.com>
2023-05-19 22:11:16 -0400

    added updated code

--- BasicJavaClass/src/main/java/Person.java
+++ BasicJavaClass/src/main/java/Person.java
@@ -1,14 +1,29 @@
 import business.Company;
 
 public class Person {
+    private String middleName = "Noman";
 
-    // Method
-    public void sayHello(){
-        System.out.println("Hello");
-    }
     public static void main(String[] args){
         Person p1 = new Person();
         p1.sayHello();
+        p1.hiWithName("Newaz");
+        System.out.println(p1.getMiddleInitial());
+        System.out.println(p1.add(5,20));
+    }
+
+    public int add (int num, int num2){
+        return num + num2;
+    }
+    public char getMiddleInitial(){
+        return middleName.charAt(0);
+    }
+
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+
+    public void hiWithName(String name){
+        System.out.println("My Name Is: "+ name);
     }
 
 }
